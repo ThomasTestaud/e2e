b4014e30abb2632dbce36f88c71c43a2f8effecd
ThomasTestaud <thomas.testaud@hotmail.fr>
2024-06-05 13:45:57 +0200

    ...

---
+++ e2e/counter.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('clicks on counter twice', async ({ page }) => {
+   
+    await page.goto('http://localhost:5173/');
+    const counterButton = await page.locator("#counter")
+
+    
+    await counterButton.click();
+    await counterButton.click();
+    await counterButton.click();
+
+    
+    const counterValue = await page.locator("#counter").innerText();
+    expect(counterValue).toBe("count is 3");
+});
